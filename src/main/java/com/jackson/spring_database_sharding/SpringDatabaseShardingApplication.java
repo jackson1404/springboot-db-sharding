85ec4e96483376eb042424c4841b4d0fbe368ce3
Win Min Thant <jackson14042004@gmail.com>
2025-07-02 15:01:40 +0630

    first commit

---
+++ src/main/java/com/jackson/spring_database_sharding/SpringDatabaseShardingApplication.java
@@ -0,0 +1,13 @@
+package com.jackson.spring_database_sharding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDatabaseShardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDatabaseShardingApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/jackson/spring_database_sharding/entity/CustomerEntity.java
@@ -0,0 +1,18 @@
+/***************************************************************
+ * Author       :	 
+ * Created Date :	
+ * Version      : 	
+ * History  :	
+ * *************************************************************/
+package com.jackson.spring_database_sharding.entity;
+
+/**
+ * CustomerEntity Class.
+ * <p>
+ * </p>
+ *
+ * @author
+ */
+
+public class CustomerEntity {
+}
